64c61a7976aae59df99723f19945d3a9b06e2b44
Sven Wirz <mails@sven-wirz.de>
2020-11-28 19:14:27 +0100

    Inital commit

---
+++ src/main/java/de/sven_wirz/spring/cloud/configuration/ConfigurationApplication.java
@@ -0,0 +1,15 @@
+package de.sven_wirz.spring.cloud.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationApplication.class, args);
+    }
+
+}
